7917b164ea81d23e2c61f1e40870f13ad2fbd40c
Bulat Cherkashin <busyokbedlam@gmail.com>
2020-02-20 16:32:19 +0200

    composite pattern finished

--- composite/class.js
+++ composite/class.js
@@ -1,24 +1,27 @@
 class Container {
-  #children;
-  #cost;
-  constructor(cost, children = null) {
+  #children
+  #cost
+  constructor(children = null, cost = null) {
     this.#children = children;
     this.#cost = cost;
   }
   getChildren() {
-    if(this.children) {
-      return this.children;
+    if(this.#children) {
+      return this.#children;
     }
     return null;
   }
 
   calculate() {
     if(this.getChildren()) {
-      return this.children.reduce((accum, curr) => { 
+      return this.#children.reduce((accum, curr) => { 
         return accum + curr.calculate();
       }, 0);
     } else {
-      return this.cost;
+      return this.#cost;
     }
   }
 }
+
+let container = new Container([new Container(null,5), new Container([new Container(null, 20),new Container(null, 20), new Container([new Container(null, 50)])])]);
+console.log(container.calculate());
